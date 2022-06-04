efc6fa7f27f62c6208c7c6539aee24ddb8e6afdd
tinnguyentg <tinnguyen121221@gmail.com>
2022-06-04 12:04:24 +0700

    add production settings

--- django_service/projectname/settings/production.py
+++ django_service/projectname/settings/production.py
@@ -1,1 +1,19 @@
+import os
+
 from .base import *  # noqa
+
+DEBUG = False
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
+CSRF_TRUSTED_ORIGINS = ["https://*" + host for host in ALLOWED_HOSTS]
+
+# Email configurations
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+# security settings
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
