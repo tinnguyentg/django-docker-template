fb7c48152cc8729aa29d1a9712df4a35e9c14a17
tinnguyentg <tinnguyen121221@gmail.com>
2022-06-04 14:13:42 +0700

    change projectname to project_name

--- django_service/project_name/celery.py
+++ django_service/project_name/celery.py
@@ -3,9 +3,9 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projectname.settings.development")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings.development")
 
-app = Celery("projectname")
+app = Celery("project_name")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
--- django_service/project_name/wsgi.py
+++ django_service/project_name/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projectname project.
+WSGI config for project_name project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projectname.settings.development")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings.development")
 
 application = get_wsgi_application()
