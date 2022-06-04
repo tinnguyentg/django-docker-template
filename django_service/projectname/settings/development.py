a9474b5cb0997ce69c4b4f83ef6da23cba303567
tinnguyentg <tinnguyen121221@gmail.com>
2022-06-04 11:48:10 +0700

    add django-debug-toolbar in development

--- django_service/projectname/settings/development.py
+++ django_service/projectname/settings/development.py
@@ -1,1 +1,7 @@
 from .base import *  # noqa
+
+INSTALLED_APPS += ["debug_toolbar"]  # noqa
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": lambda request: True,
+}
